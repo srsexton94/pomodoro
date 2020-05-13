dce33b828cd74c3c540924527b6667f1e194580e
srsexton94 <srsexton94@gmail.com>
2020-05-12 21:21:17 -0400

    Write function that will start/stop once
    Need to make work repeatedly, running into issues with setInterval and
    clearInterval

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,42 @@
 'use strict'
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
+// const bell = new Audio('./public/bell.mp3')
+let minInterval
+let secInterval
 
-// use require without a reference to ensure a file is bundled
-// require('./example')
+const minTimer = () => {
+  const min = parseInt($('.minutes').text())
+  $('.minutes').text(min - 1)
+}
+
+const secTimer = () => {
+  let sec = parseInt($('.seconds').text())
+  if (sec === 0) {
+    sec = 60
+  }
+  $('.seconds').text(sec - 1)
+}
+
+const timeControls = event => {
+  const id = event.target.id
+  if (id === 'start') {
+    $('.start-pause').toggleClass('hidden')
+    minInterval = setInterval(minTimer, 60000)
+    secInterval = setInterval(secTimer, 1000)
+  } else {
+    clearInterval(minInterval)
+    clearInterval(secInterval)
+  }
+  console.log(id)
+}
+
+const onReset = event => {
+  $('.minutes').text('25')
+  $('.seconds').text('00')
+  console.log('Reset')
+}
 
 $(() => {
-  // your JS code goes here
+  $('.start-pause').on('click', timeControls)
+  $('#reset').on('click', onReset)
 })
