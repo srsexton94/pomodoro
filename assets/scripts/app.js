d05572070e396dd8959a80545691984576ca808c
srsexton94 <srsexton94@gmail.com>
2020-05-12 21:32:19 -0400

    Write logic for working Start, Pause, and Resets
    Write the logic to handle start, pause, and reset functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -17,26 +17,31 @@ const secTimer = () => {
   $('.seconds').text(sec - 1)
 }
 
-const timeControls = event => {
-  const id = event.target.id
-  if (id === 'start') {
-    $('.start-pause').toggleClass('hidden')
+const startPauseInterval = (bool) => {
+  $('.start-pause').toggleClass('hidden')
+  if (bool) {
     minInterval = setInterval(minTimer, 60000)
     secInterval = setInterval(secTimer, 1000)
   } else {
     clearInterval(minInterval)
     clearInterval(secInterval)
   }
-  console.log(id)
 }
 
 const onReset = event => {
+  startPauseInterval(false)
   $('.minutes').text('25')
   $('.seconds').text('00')
-  console.log('Reset')
 }
 
 $(() => {
-  $('.start-pause').on('click', timeControls)
+  $('#start').on('click', event => {
+    event.preventDefault()
+    startPauseInterval(true)
+  })
+  $('#pause').on('click', event => {
+    event.preventDefault()
+    startPauseInterval(false)
+  })
   $('#reset').on('click', onReset)
 })
